06a857ca690fd960af76fbc24cbd69ae60d5db30
Iago Freire <iagojfreire@gmail.com>
2026-05-25 21:10:06 -0300

    Creating class level

--- code/Level.py
+++ code/Level.py
@@ -1,11 +1,23 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import pygame
+from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.Player import Player
+
 
 class Level:
-    def __init__(self):
-        self.window = None
-        self.name = None
-        self.entity_list = None
+    def __init__(self, window):
+        self.window = window
+        self.player = Player()
 
-    def run(self, ):
-        pass
+    def run(self, eventos):
+        # Preenche a tela com um cinza escuro simulando o asfalto
+        self.window.fill((60, 60, 60))
+
+        # Escuta os comandos do teclado dentro do Level
+        for event in eventos:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.player.pular()
+
+        # Atualiza a física e desenha o jogador na tela
+        self.player.update()
+        self.player.draw(self.window)
