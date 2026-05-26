d9b846fb06f27fd811e9b86a0312d79382225dea
Iago Freire <iagojfreire@gmail.com>
2026-05-25 21:23:39 -0300

    updating const class

--- code/const.py
+++ code/const.py
@@ -6,6 +6,7 @@ WIN_HEIGHT = 600
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 COLOR_BLUE = (50, 150, 255)
+# COLOR_GREEN = (0, 200, 100) # Removido para simplificar, se não for usar. Se usar, mantenha.
 COLOR_GREEN = (0, 200, 100)
 
 # Estados do Jogo
@@ -13,3 +14,6 @@ MENU_STATE = 0
 LEVEL_STATE = 1
 GAME_OVER_STATE = 2
 VICTORY_STATE = 3
+
+# Configurações de Gameplay
+TARGET_DISTANCE = 5000
