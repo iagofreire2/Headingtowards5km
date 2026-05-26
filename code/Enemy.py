c645e6ea7e8a8849cce06d525018061f1187cf24
Iago Freire <iagojfreire@gmail.com>
2026-05-25 21:17:37 -0300

    Creating enemy class

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from Entity import Entity
-
+from code.Entity import Entity
+from code.const import WIN_WIDTH
 
 class Enemy(Entity):
     def __init__(self):
-        pass
+        # Chama o construtor da superclasse (largura 40, altura 40, cor vermelha)
+        super().__init__(WIN_WIDTH, 450, 40, 40, (255, 50, 50))
+        self.velocidade_x = -7 # Move para a esquerda
 
-    def move(self, ):
-        pass
+    def update(self):
+        self.rect.x += self.velocidade_x
+
