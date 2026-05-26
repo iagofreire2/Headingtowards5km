72af086fd44b361f50da6ff93f430d4aee3b2b02
Iago Freire <iagojfreire@gmail.com>
2026-05-25 21:18:23 -0300

    updating level class

--- code/Level.py
+++ code/Level.py
@@ -1,23 +1,46 @@
 import pygame
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.const import WIN_WIDTH, WIN_HEIGHT, LEVEL_STATE, GAME_OVER_STATE
 from code.Player import Player
+from code.Enemy import Enemy
 
 
 class Level:
     def __init__(self, window):
         self.window = window
         self.player = Player()
+        self.enemies = []  # Lista para guardar os obstáculos
+        self.spawn_timer = 0  # Cronômetro para gerar novos obstáculos
 
     def run(self, eventos):
-        # Preenche a tela com um cinza escuro simulando o asfalto
         self.window.fill((60, 60, 60))
 
-        # Escuta os comandos do teclado dentro do Level
+        # 1. Controles do Jogador
         for event in eventos:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.player.pular()
 
-        # Atualiza a física e desenha o jogador na tela
         self.player.update()
         self.player.draw(self.window)
+
+        # 2. Lógica de Geração de Obstáculos (Spawn)
+        self.spawn_timer += 1
+        if self.spawn_timer > 90:  # A cada 90 frames (1.5 segundos), cria um inimigo
+            self.enemies.append(Enemy())
+            self.spawn_timer = 0
+
+        # 3. Atualização e Colisão
+        for enemy in self.enemies:
+            enemy.update()
+            enemy.draw(self.window)
+
+            # Remove o inimigo se ele sair da tela pela esquerda
+            if enemy.rect.x < -50:
+                self.enemies.remove(enemy)
+
+            # Verifica a Colisão (Condição de Derrota)
+            if self.player.rect.colliderect(enemy.rect):
+                print("BATEU!")
+                return GAME_OVER_STATE  # Avisa que deu Game Over
+
+        return LEVEL_STATE  # Se não bateu, continua no estado de jogo
